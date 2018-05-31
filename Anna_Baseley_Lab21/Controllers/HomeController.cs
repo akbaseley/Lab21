e8a22c7ee384d0cc11013f9850643c104a011255
Anna B <akbaseley@gmail.com>
2018-05-31 16:55:16 -0400

    Second Commit of GC Coffee Shop - Lab 23

--- Anna_Baseley_Lab21/Controllers/HomeController.cs
+++ Anna_Baseley_Lab21/Controllers/HomeController.cs
@@ -11,6 +11,10 @@ namespace Anna_Baseley_Lab21.Controllers
     {
         public ActionResult Index()
         {
+            CoffeeShopDBEntities ORM = new CoffeeShopDBEntities();
+
+            ViewBag.ItemList = ORM.Items.ToList();
+
             return View();
         }
 
@@ -32,16 +36,124 @@ namespace Anna_Baseley_Lab21.Controllers
         {
             return View();
         }
-        public ActionResult AddUser(UserInfo newUser)
+
+        public ActionResult AddUser(User newUser)
         {
             if (ModelState.IsValid)
             {
-                ViewBag.Message = $"Hello,{newUser.FirstName}!";
+                CoffeeShopDBEntities ORM = new CoffeeShopDBEntities();
+                ORM.Users.Add(newUser);
+                ORM.SaveChanges();
+
+                ViewBag.Message = $"Hello,{newUser.First_Name}!";
                 return View("Confirm");
             }
             else
             {
-                ViewBag.Address = Request.UserHostAddress;
+                ViewBag.Message = "Oops! It looks as though something has gone wrong!";
+                return View("Error");
+            }
+        }
+
+        public ActionResult SearchByItemName(string item)
+        {
+            CoffeeShopDBEntities ORM = new CoffeeShopDBEntities();
+
+            ViewBag.ItemList = ORM.Items.Where(i=> i.Name.Contains(item)).ToList();
+
+            return View("Index");
+        }
+
+        public ActionResult ItemAdmin()
+        {
+            CoffeeShopDBEntities ORM = new CoffeeShopDBEntities();
+
+            ViewBag.ItemList = ORM.Items.ToList();
+
+            return View();
+        }
+
+        public ActionResult DeleteItem(string Name)
+        {
+            CoffeeShopDBEntities ORM = new CoffeeShopDBEntities();
+
+            Item Found = ORM.Items.Find(Name);
+
+            if(Found != null)
+            {
+                ORM.Items.Remove(Found);
+
+                ORM.SaveChanges();
+
+                return RedirectToAction("ItemAdmin");
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Item not found!";
+                return View("Error");
+            }
+        }
+
+        public ActionResult ShowItemDetails(string Name)
+        {
+            CoffeeShopDBEntities ORM = new CoffeeShopDBEntities();
+
+            Item Found = ORM.Items.Find(Name);
+
+            if (Found != null)
+            {
+                return View(Found);
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "The Item was not found";
+                return View("Error");
+            }
+        }
+
+        public ActionResult SaveItemUpdates(Item updatedItem)
+        {
+            CoffeeShopDBEntities ORM = new CoffeeShopDBEntities();
+
+            Item oldItem = ORM.Items.Find(updatedItem.Name);
+
+            if(oldItem !=null && ModelState.IsValid)
+            {
+                oldItem.Description = updatedItem.Description;
+                oldItem.Quantity = updatedItem.Quantity;
+                oldItem.Price = updatedItem.Price;
+
+                ORM.Entry(oldItem).State = System.Data.Entity.EntityState.Modified;
+
+                ORM.SaveChanges();
+
+                return RedirectToAction("ItemAdmin");
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Oops!  Something went wrong!";
+                return View("Error");
+            }
+        }
+
+        public ActionResult ItemForm()
+        {
+            return View();
+        }
+
+        public ActionResult AddNewItem(Item newItem)
+        {
+            if (ModelState.IsValid)
+            {
+                CoffeeShopDBEntities ORM = new CoffeeShopDBEntities();
+                ORM.Items.Add(newItem);
+                ORM.SaveChanges();
+
+                return RedirectToAction("ItemAdmin");
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Oops! It does not look as if something went wrong!";
                 return View("Error");
             }
         }
